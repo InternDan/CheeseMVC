4316f8bea838606a6fa4b91afdf05fc08b072c4c
InternDan <daniel.leib@gmail.com>
2018-06-03 15:36:33 -0500

    First push

---
+++ src/CheeseMVC/Controllers/CheeseController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CheeseMVC.Models;
+using System.Collections.Generic;
+using CheeseMVC.ViewModels;
+
+namespace CheeseMVC.Controllers
+{
+    public class CheeseController : Controller
+    {
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            List<Cheese> cheeses = CheeseData.GetAll();
+
+            return View(cheeses);
+        }
+
+        public IActionResult Add()
+        {
+            AddCheeseViewModel addCheeseViewModel = new AddCheeseViewModel();
+            return View(addCheeseViewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Add(AddCheeseViewModel addCheeseViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                // Add the new cheese to my existing cheeses
+                Cheese newCheese = new Cheese
+                {
+                    Name = addCheeseViewModel.Name,
+                    Description = addCheeseViewModel.Description,
+                    Type = addCheeseViewModel.Type
+                };
+
+                CheeseData.Add(newCheese);
+
+                return Redirect("/Cheese");
+            }
+
+            return View(addCheeseViewModel);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int cheeseId)
+        {
+            Cheese cheese = CheeseData.GetById(cheeseId);
+            ViewBag.cheese = cheese;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Edit(int cheeseId, string name, string description)
+        {
+            var cheeses = CheeseData.GetAll();
+            cheeses[cheeseId-1].Name = name;
+            cheeses[cheeseId-1].Description = description;
+            return Redirect("Index");
+        }
+
+        public IActionResult Remove()
+        {
+            ViewBag.title = "Remove Cheeses";
+            ViewBag.cheeses = CheeseData.GetAll();
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Remove(int[] cheeseIds)
+        {
+            foreach (int cheeseId in cheeseIds)
+            {
+                CheeseData.Remove(cheeseId);
+            }
+
+            return Redirect("/");
+        }
+    }
+}
